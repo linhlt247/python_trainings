383df26caed7ce9da52d56b8b8c3297e1c0394f0
Totoro <totoro.github@gmail.com>
2016-11-19 21:11:36 +0700

    task10

--- task10/pylevel6.py
+++ task10/pylevel6.py
@@ -1,19 +1,17 @@
 #-*- coding: utf-8 -*-
 import zipfile,re
-def find_result():
+def find():
 	source = zipfile.ZipFile('channel.zip') # mở file zip
-	number = '90052' #file readme.txt gợi ý mở file 90052.txt
-	text = 'Next nothing is (\d+)' #thông điệp trong file
-	comments = "" 
+	number = ['90052'] # file đầu tiên cần mở theo gợi ý trong file readme.txt ở trong file zip
+	comment = ''
 	while True:
-		try:
-			txt = source.read(number + '.txt') #đọc nội dung trong file number.txt
-			number = re.search(text,txt).group(1) #tìm trong nội dung file và number là số hiệu của file tiếp theo
-		except:
-			print txt #in ra kết quả cuối cùng :Collect the comments
+		text = source.read(number[0]+'.txt') #đọc nội dung của file trong file zip
+		comment += source.getinfo(number[0]+'.txt').comment #getinfo(): trả lại một đối tượng Zipinfo với thông tin về thành viên trong file zip, comment là phần gắn liền với file zip, nó thuộc phần thông tin của file trong file zip đó.
+		if 'Next' in text:
+			number = re.findall("[0-9]+",text) #lấy số trong nội dung file để mở file tiếp theo
+		else:
+			print text
 			break
-		comments = comments + source.getinfo(number + '.txt').comment #thu thập comments
-	print comments
-
+	print comment
 if __name__ == '__main__':
-	find_result()
+	find()
